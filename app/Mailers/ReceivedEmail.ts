195dbef9c38cdc3aee57ac1a58d0746d08db09c9
vigreux-joel <vigreux.joel@hotmail.com>
2022-05-18 14:21:50 +0200

    snow

--- app/Mailers/ReceivedEmail.ts
+++ app/Mailers/ReceivedEmail.ts
@@ -1,22 +1,30 @@
 import { BaseMailer, MessageContract } from '@ioc:Adonis/Addons/Mail'
 import View from '@ioc:Adonis/Core/View'
 import mjml from 'mjml'
+//Imports mail type
+import { Email } from 'App/Controllers/Http/MailController'
 
 export default class ReceivedEmail extends BaseMailer {
+  //Recovers the view containing the mjml tag using the email object as a parameter
   public html = View.render('emails/received', {
-    email: this.email
+    email: this.email,
   })
 
-  constructor(private email: object) {
-    super();
+  //uses the constructor of the parent class and initializes the email property
+  constructor(private email: Email) {
+    super()
   }
 
-  public prepare(message: MessageContract) {
+  /*Method used when initializing an instance, it allows to send the email by defining:
+    the subject, the sender, the recipient and the content using MJML,
+    and thus to send an email.
+    */
+  public prepare(message: MessageContract): void {
     this.html.then((vue) => {
       message
-        .subject('Votre a bien été recu!')
+        .subject('Votre email a bien été recu!')
         .from('contact@vigreux-joel.fr', 'Vigreux Joël - Développeur web')
-        .to('vigreux.joel@hotmail.com')
+        .to(this.email.sender)
         .html(mjml(vue).html)
     })
   }
--- resources/js/home.js
+++ resources/js/home.js
@@ -5,6 +5,10 @@ window.addEventListener('scroll', () => {
     observer.observe(section)
   })
 
+  moveYElement()
+})
+moveYElement()
+function moveYElement() {
   const elements = document.querySelectorAll('.move-y')
   elements.forEach((element) => {
     const parent = element.parentNode
@@ -21,7 +25,7 @@ window.addEventListener('scroll', () => {
     let amplifier = element.dataset.amplifier ?? 1
     element.style.top = `${diff * amplifier}px`
   })
-})
+}
 
 observer = new IntersectionObserver(
   function (entries) {
@@ -36,8 +40,6 @@ observer = new IntersectionObserver(
     threshold: [0.6],
   }
 )
-
-
 
 const links = document.querySelectorAll('header .links a')
 const linksAfter = document.querySelector('header .links .after')
@@ -57,30 +59,64 @@ function setCurrentOnglet(id) {
   }
 }
 
-//projet
-document.querySelectorAll('#projets article .images').forEach((images) => {
-  images.onclick = () => {
-    images.classList.toggle('active')
+//snow
+let snowCount = 75
+
+function createSnow(snow_density) {
+  for (let x = 0; x < snow_density - 1; x++) {
+    let board = document.createElement('div')
+    board.className = 'snow'
+    document.getElementById('snow-div').appendChild(board)
   }
-})
-
-//competance transform: rotate3d(49, 190, 56, -69deg);
-// }
-const scrollDiv = document.querySelector('#competances .scroll')
-onScroll()
-console.log('reerggre')
-scrollDiv.parentNode.onscroll = function () {
-  onScroll()
 }
 
-function onScroll() {
-  let x = -1
-  let y = -1
-  let z = Math.abs(8)
-
-  scrollDiv.style.transform = `rotate3d(${x}, ${y}, ${z}, -30deg) translate3d(-78px, -85px, 0px)`
-  const images = scrollDiv.querySelectorAll('img')
-  images.forEach((image) => {
-    image.style.transform = `rotate3d(${Math.abs(x)}, ${Math.abs(y)}, ${Math.abs(z)}, -30deg)`
-  })
+// Append style for each snowflake to the head
+function add_css(rule) {
+  const css = document.createElement('style')
+  css.type = 'text/css'
+  css.appendChild(document.createTextNode(rule)) // Support for the rest
+  document.getElementsByTagName('head')[0].appendChild(css)
 }
+
+// Math
+function randomRange(min = 0, max = 1) {
+  return Math.random() * (max - min) + min
+}
+
+// Create style for snowflake
+function createSnowCSS(snow_density) {
+  let rule = ``
+  for (let i = 1; i < snow_density; i++) {
+    let xDefault = randomRange(0, 100)
+    let xRange = randomRange(-15, 15)
+    let yBreak = randomRange(30, 80) * 100
+    let fallDuration = randomRange(8, 30)
+    let fallDelay = randomRange(8, 30)
+
+    rule += `
+        .snow:nth-child(${i}) {
+            opacity: ${randomRange(0, 0.75)};
+            transform: translate(${xDefault}vw, -10px) scale(${Math.random()});
+            animation: fall-${i} ${fallDuration}s ${fallDelay}s linear infinite;
+        }
+
+        @keyframes fall-${i} {
+            ${yBreak * 100}% {
+                opacity: ${randomRange(0, 0.75)};
+                transform: translate(${xDefault + xRange}vw, ${yBreak}vh) scale(${Math.random()});
+            }
+
+            to {
+                opacity: ${randomRange(0, 0.75)};
+                transform: translate(${xDefault + xRange / 2}vw, 100vh) scale(${Math.random()});
+            }
+
+        }
+        `
+  }
+
+  add_css(rule)
+}
+
+createSnowCSS(snowCount)
+createSnow(snowCount)
