ee3766c42a7b9646c02caad5dea4fc1f80ca3ec4
vigreux-joel <vigreux.joel@hotmail.com>
2022-06-23 23:10:21 +0200

    test https

--- server.ts
+++ server.ts
@@ -17,3 +17,24 @@ import { Ignitor } from '@adonisjs/core/build/standalone'
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
 new Ignitor(__dirname).httpServer().start()
+
+// import 'reflect-metadata'
+// import sourceMapSupport from 'source-map-support';
+// import { join } from 'path';
+// import { readFileSync } from 'fs';
+// import { createServer } from 'https';
+// import { Ignitor } from '@adonisjs/core/build/src/Ignitor';
+//
+// sourceMapSupport.install({ handleUncaughtExceptions: false })
+//
+// const privateKey = readFileSync(join(__dirname + '/sslCert/server.key'), 'utf8');
+// const certificate = readFileSync(join(__dirname + '/sslCert/server.crt'), 'utf8');
+// const credentials = {key: privateKey, cert: certificate};
+//
+// new Ignitor(__dirname)
+//   .httpServer()
+//   .start((handle) => {
+//     return createServer(credentials, handle);
+//   })
+//   .catch(console.error)
+
