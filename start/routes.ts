f3730ea33c1a1593b566658b3cd08f8541c7b08b
vigreux-joel <vigreux.joel@hotmail.com>
2022-06-24 10:07:58 +0200

    seo

--- start/routes.ts
+++ start/routes.ts
@@ -23,9 +23,9 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'HomeController.index').as('home')
-  // Route.get('*', async ({ request,response }) => {
-  //   response.redirect().toPath('http://' + <string>request.host() /*+ request.url()*/)
-  // })
+  Route.get('*', async ({ request,response }) => {
+    response.redirect().toPath('http://' + <string>request.host() /*+ request.url()*/)
+  })
   Route.post('/contact', 'MailController.index').as('mail')
 })
 /*  .as('web')
