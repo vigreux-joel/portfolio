05d8063c96c359c86297a3a2ec6936b7dfdcca6d
vigreux-joël <vigreux.joel@hotmail.com>
2024-08-08 11:24:20 +0200

    feat: enhance theming and update Link component
    Updated theme colors and variant configurations in `theme.config.ts` for better consistency. Added className prop to Link component and improved its styling. Revised dependencies in `package.json` and `package-lock.json` for latest package versions and implemented new contact section in `index.astro`.

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,9 +1,14 @@
-export const Link = ({children, href, target}: { children: React.ReactNode, href: string, target?: string }) => {
+export const Link = ({children, href, target, className}: {
+    children: React.ReactNode,
+    href: string,
+    target?: string,
+    className?: string
+}) => {
 
     return <a
-        className="text-secondary group rounded overflow-hidden relative focus-visible:text-primary hover:text-primary"
+        className={"text-secondary group rounded overflow-hidden relative focus-visible:text-primary hover:text-primary " + className}
         href={href} target={target}>
        <span
-           className={"underline-offset-4 underline "}>{children}</span>
+           className={"underline-offset-4 underline !text-bold"}>{children}</span>
     </a>
 }
