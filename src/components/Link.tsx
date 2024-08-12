ce52e3d57817bfd5b200348adf6c28bd635f958d
vigreux-joël <vigreux.joel@hotmail.com>
2024-08-12 19:29:04 +0200

    feat: remove unused components and streamline configuration
    Deleted unused components: AuroraBackground, BorderBeam, TextGenerateEffect, TextRevealByWord, and Vortex. Streamlined configurations in Techno, Link components, and astro.config.mjs for better performance. Added astro-compress package for enhanced compression.

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,14 +1,24 @@
-export const Link = ({children, href, target, className}: {
-    children: React.ReactNode,
-    href: string,
-    target?: string,
-    className?: string
+export const Link = ({
+  children,
+  href,
+  target,
+  className,
+}: {
+  children: React.ReactNode;
+  href: string;
+  target?: string;
+  className?: string;
 }) => {
-
-    return <a
-        className={"text-secondary group rounded overflow-hidden relative focus-visible:text-primary hover:text-primary " + className}
-        href={href} target={target}>
-       <span
-           className={"underline-offset-4 underline !text-bold"}>{children}</span>
+  return (
+    <a
+      className={
+        "text-secondary group rounded overflow-hidden relative focus-visible:text-primary hover:text-primary underline-offset-4 underline !text-bold" +
+        className
+      }
+      href={href}
+      target={target}
+    >
+      {children}
     </a>
-}
+  );
+};
