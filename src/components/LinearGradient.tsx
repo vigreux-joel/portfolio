2d091341397d0be7f3ff0a463b75f87520bbb7b5
Joël VIGREUX <joel@udixio.fr>
2026-08-03 15:30:22 +0200

    feat(home): refine sections with updated visuals and BackgroundColor effects
    Enhance "Travailler ensemble," "Projects," and "Expertise Preview" with `BackgroundColor` for dynamic visuals. Adjust layouts, z-index values, and class usage for better layering and consistency. Simplify structural styles in FAQ and global styles.

--- src/components/LinearGradient.tsx
+++ src/components/LinearGradient.tsx
@@ -7,7 +7,7 @@ className?: string
 
     return (
         <div
-            className={classNames("w-screen -z-10 h-64 pointer-events-none absolute bottom-0 bg-gradient-to-b left-0 from-transparent to-surface",`theme-${nextTheme}`, className) }>
+            className={classNames("w-screen -z-20 h-64 pointer-events-none absolute bottom-0 bg-gradient-to-b left-0 from-transparent to-surface",`theme-${nextTheme}`, className) }>
         </div>
     )
 }
